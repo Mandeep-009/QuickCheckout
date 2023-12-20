9296b3b0b3b0c311bdd2eb3258f60c0f29780ef0
Mandeep <mandeepgahlawat009@gmail.com>
2023-12-20 07:35:21 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,27 +5,53 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const apiKey = `${process.env.API_KEY}`
-const authDomain = `${process.env.AUTH_DOMAIN}`
-const databaseURL = `${process.env.DB_URL}`
-const projectId = `${process.env.PROJECT_ID}`
-const storageBucket = `${process.env.STORAGE_BUCKET}`
-const messagingSenderId = `${process.env.MESSAGING_SENDER_ID}`
-const appId = `${process.env.APP_ID}`
-const measurementId = `${process.env.MEASUREMENT_ID}`
+// const apiKey = `${process.env.API_KEY}`
+// const authDomain = `${process.env.AUTH_DOMAIN}`
+// const databaseURL = `${process.env.DB_URL}`
+// const projectId = `${process.env.PROJECT_ID}`
+// const storageBucket = `${process.env.STORAGE_BUCKET}`
+// const messagingSenderId = `${process.env.MESSAGING_SENDER_ID}`
+// const appId = `${process.env.APP_ID}`
+// const measurementId = `${process.env.MEASUREMENT_ID}`
   
+// const firebaseConfig = {
+//     apiKey,
+//     authDomain,
+//     databaseURL,
+//     projectId,
+//     storageBucket,
+//     messagingSenderId,
+//     appId,
+//     measurementId
+// }
+
 const firebaseConfig = {
-    apiKey,
-    authDomain,
-    databaseURL,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
-    measurementId
-}
+
+    apiKey: "AIzaSyAW9jWu21SHpjunQMTNvzc7OcLn19dZY_Q",
+  
+    authDomain: "receipt-system-12689.firebaseapp.com",
+  
+    databaseURL: "https://receipt-system-12689-default-rtdb.asia-southeast1.firebasedatabase.app",
+  
+    projectId: "receipt-system-12689",
+  
+    storageBucket: "receipt-system-12689.appspot.com",
+  
+    messagingSenderId: "145988567998",
+  
+    appId: "1:145988567998:web:474061542e89226b38efef",
+  
+    measurementId: "G-2ZNSFBKKPP"
+  
+  };
+  
+  
 
 const port = process.env.PORT || 5186;
+
+app.get('/',(req,res)=>{
+    res.send('hello guys....');
+})
 
 app.get('/firebase-config',(req,res)=>{
     res.send(firebaseConfig);
